da9e89afc184a27f39d4112ffff1c15a6c7b87ec
AnH13-6 <60193027+AnH13-6@users.noreply.github.com>
2022-09-19 12:25:02 +0200

    add entry form for new vacations (#5)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,13 @@
 import {GlobalStyle} from '../styles';
 
+import VacationForm from './VacationForm';
+
 export default function App({Component, pageProps}) {
 	return (
 		<>
 			<GlobalStyle />
 			<Component {...pageProps} />
+			<VacationForm />
 		</>
 	);
 }
