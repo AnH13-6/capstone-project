94792cf90152ee51d868262756bc1ac19330da7c
Anne Hölscher <anne.k.hoelscher@gmail.com>
2022-09-20 14:15:41 +0200

    add useVacationStore with zustand

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,15 @@
+import VacationForm from '../components/VacationForm';
+import Vacations from '../components/vacations';
 import {GlobalStyle} from '../styles';
-
-import VacationForm from './VacationForm';
 
 export default function App({Component, pageProps}) {
 	return (
 		<>
 			<GlobalStyle />
 			<Component {...pageProps} />
+			<h1>My Vacations</h1>
 			<VacationForm />
+			<Vacations />
 		</>
 	);
 }
